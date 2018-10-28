e9957886e6a2c6e0ec91edc1792f24935d0b4669
WenlinG28 <41845347+WenlinG28@users.noreply.github.com>
2018-10-28 13:17:58 -0400

    Add files via upload

---
+++ Assets/firstpersoncamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstpersoncamera : MonoBehaviour {
+        float speed = 6.0f;
+        float rspeed = 6.0f;
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+
+
+	
+	
+	void Update () {
+	
+
+       
+        CharacterController controller = (CharacterController)base.GetComponent(typeof(CharacterController));
+       
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rspeed, 0);
+       
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        
+        controller.SimpleMove(forward * curSpeed);
+
+       	        
+
+	}
+}
