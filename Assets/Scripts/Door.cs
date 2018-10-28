26b38e841f90b37363bcfb81b2c50af9358178e2
WenlinG28 <41845347+WenlinG28@users.noreply.github.com>
2018-10-28 16:36:46 -0400

    Add files via upload

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public Animation anim;
+    private int m_LastIndex;
+	public void PlayDoorAnim () {
+        if (!anim.isPlaying)
+        {
+            if(m_LastIndex == 0)
+            {
+                anim.Play("DoorOpen");
+                m_LastIndex = 1;
+            } else
+            {
+                anim.Play("DoorClose");
+                m_LastIndex = 0;
+            }
+        }
+	}
+}
