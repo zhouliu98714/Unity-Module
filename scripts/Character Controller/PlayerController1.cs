a34b88e2c9184035ecdb15568265cd578ba63898
Jiawen <linj14@rpi.edu>
2018-11-08 14:07:16 -0500

    Player controller

---
+++ scripts/Character Controller/PlayerController1.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// At this point, this code is not compeletely done. 
+// The player is not contraint to the ground (when moving with key controls)
+// Jumping works, however, not contraint to the ground as well.
+
+// The good thing is, we can fly in our bookstore!!! :)
+
+public class PlayerController1 : MonoBehaviour
+{
+
+
+    public float speed = 5;
+    private Rigidbody rb;
+    public LayerMask groundLayers;
+
+    public float jumpForce = 7; // applying mathematical calculation to jump
+    public SphereCollider col;
+    // Use this for initialization
+
+    #region Monobehaviour API
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+
+        // if we press the space
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    // use this function to make sure that we don't jump infinitely -- once we hits the ground, we stop.
+    private bool IsGrounded(){
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, 
+         col.bounds.center.z), col.radius * .9f, groundLayers);
+    }
+    #endregion
+}
---
+++ scripts/Character Controller/README.md
@@ -0,0 +1,8 @@
+* Introduction
+
+This is a code designed to render player controls, including but are not limited to (will be implemented further in the future) basic movements, jumping, and currently flying due to the failure of constraint to the ground.
+
+
+* Requirements
+Needs to be placed inside player object, along with Mesh Renderer, Rigidbody, and Sphere collider. 
+Make sure to match the public variable "Ground Layer" in this code with the layer of the player object!
