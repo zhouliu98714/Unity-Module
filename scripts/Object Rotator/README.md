9a40dd1d613a1ffcb141fe8d74badb3a7a14dfed
Jiawen <linj14@rpi.edu>
2018-11-08 14:11:58 -0500

    object rotation

---
+++ scripts/Object Rotator/README.md
@@ -0,0 +1,11 @@
+* Introduction
+
+This is a code designed to render infinite object rotation
+
+
+* Requirements
+Needs to be placed inside player object, along with Mesh Renderer, Rigidbody, and Box collider. 
+
+
+* Future Implementation
+Will consider implementing finite rotation under a given amount of time or requirements.
---
+++ scripts/Object Rotator/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Performs inifinite rotation
+public class Rotator : MonoBehaviour {
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(13, 30, 45) * Time.deltaTime);
+	}
+}
