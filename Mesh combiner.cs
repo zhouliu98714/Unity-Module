6a8fe3e6a9262bc2d97388b9198b7f5734f8f8b6
pangtsu <42752128+pangtsu@users.noreply.github.com>
2018-12-08 02:27:19 -0500

    Mesh combiner
    These two scripts combined aim for optimization in game performances and scene construction. They combine different meshes to one big mesh (i.e., table stands+plank = table) in order to reduce lagginess.

---
+++ Mesh combiner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+// a script to combine all the meshes for optimizing game performance
+namespace Battlehub.Integration
+{
+    public static class MeshCombinerIntegration 
+    {
+        public static event IntegrationHandler Combined;
+        public static event IntegrationHandler BeginEditPivot;
+       
+        public static void RaiseCombined(GameObject go, Mesh mesh)
+        {
+            if(Combined != null)
+            {
+                Combined(new IntegrationArgs(go, mesh));
+            }
+        }
+
+        public static bool RaiseBeginEditPivot(GameObject go, Mesh mesh)
+        {
+            if(BeginEditPivot != null)
+            {
+                IntegrationArgs args = new IntegrationArgs(go, mesh);
+                BeginEditPivot(args);
+                return !args.Cancel;
+            }
+
+            return true;
+        }
+
+
+    }
+}
+
