ce7b29bf799bd7ca581b7dcdd4465c9ccb108942
pangtsu <42752128+pangtsu@users.noreply.github.com>
2018-12-08 02:25:18 -0500

    script for one object to follow another
    This script allows a certain object to follow another. In this case, the camera would be set to a specific position and follow the player everywhere.

---
+++ Follow object.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This is a simple code used for "following a certain object". I used this to create the
+// third person camera. 
+/*!
+ *	----------------------------------------------------------------------
+ *	
+*/
+public class SimpleFollowCam : MonoBehaviour
+{
+	[SerializeField] private Transform	m_Target		= null;
+	[SerializeField] private float		m_FollowSpeed	= 6.0f;
+
+
+	/*!
+	*/
+//	private void Start()
+//	{
+//	}
+
+	/*!
+	 *	----------------------------------------------------------------------
+	*/
+	private void FixedUpdate()
+	{
+		if( null == m_Target ) return;
+
+		float followSpeed = (m_FollowSpeed * Time.deltaTime);
+		Vector3 pos = this.transform.position;
+		pos = Vector3.Lerp( pos, m_Target.position, followSpeed );
+		this.transform.position = pos;
+	}
+
+
+}
